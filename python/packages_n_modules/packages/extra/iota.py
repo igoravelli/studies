062cd8c140fd041e0cfd7f4c7aee90bd06228be3
igorravelli <igorregly@gmail.com>
2023-08-28 22:50:47 -0300

    working with packages

---
+++ python/packages_n_modules/packages/extra/iota.py
@@ -0,0 +1,7 @@
+def funI():
+    print("Im the function I from extra folder")
+
+
+
+if __name__ == "__main__":
+    print('I prefer to be a module')
---
+++ python/packages_n_modules/progs/main2.py
@@ -0,0 +1,8 @@
+from sys import path
+path.append('/home/igor/studies/python/packages_n_modules/packages')
+
+
+
+import extra.iota
+
+print(extra.iota.funI())
